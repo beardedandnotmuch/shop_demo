c47ea643506a626128dc8187dc9bbfda535baca0
rkalita <exit2nowhere@gmail.com>
2017-03-03 15:01:25 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { MdDialog } from '@angular/material';
+import { BasketComponent } from './basket/basket.component';
+import { Product } from './product/shared/product'
+import { ProductService } from "./product/shared/product.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  products: Array<Product>
+  constructor(
+    public dialog: MdDialog,
+    public ProductService: ProductService
+  ) {
+    this.products = ProductService.items;
+  }
+
+  openBasket() {
+    let dialogRef = this.dialog.open(BasketComponent, {
+      width: '300px',
+      height: 'auto'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule, AuthMethods, AuthProviders } from "angularfire2";
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { BasketComponent } from './basket/basket.component';
+import { ProductService } from "./product/shared/product.service";
+
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent
+  },
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByJ0HdLVBOlC6Y9CQDwNepdEuLfjdd4Bc",
+  authDomain: "tittle-tattle-faf84.firebaseapp.com",
+  databaseURL: "https://tittle-tattle-faf84.firebaseio.com",
+  storageBucket: "tittle-tattle-faf84.appspot.com"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    BasketComponent
   ],
+  entryComponents: [ BasketComponent ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    MaterialModule.forRoot(),
+    FlexLayoutModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig,{
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    })
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
