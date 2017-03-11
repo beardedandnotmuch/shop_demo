01ed49a92c40bc46c46d5304b9c58a034992447b
rkalita <exit2nowhere@gmail.com>
2017-03-11 15:43:38 +0700

    routing listener and preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,15 @@ import { MdDialog } from '@angular/material';
 import { BasketComponent } from './basket/basket.component';
 import { Product } from './product/shared/product'
 import { ProductService } from "./product/shared/product.service";
+import {
+  ActivatedRouteSnapshot,
+  Event as RouterEvent,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +20,16 @@ import { ProductService } from "./product/shared/product.service";
 })
 export class AppComponent {
   products: Array<Product>;
+  isLoading: boolean = true;
   constructor(
     public dialog: MdDialog,
-    public ProductService: ProductService
+    public ProductService: ProductService,
+    private router: Router
   ) {
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event);
+    });
+
     this.products = ProductService.items;
   }
 
@@ -27,5 +42,26 @@ export class AppComponent {
     dialogRef.afterClosed().subscribe(result => {
       console.log(result);
     });
+  }
+
+  /**
+   * Shows and hides the loading spinner during RouterEvent changes
+   *
+   * @param {RouterEvent} event
+   * @memberOf AppComponent
+   */
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.isLoading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.isLoading = false;
+    }
+    if (event instanceof NavigationCancel) {
+      this.isLoading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.isLoading = false;
+    }
   }
 }
